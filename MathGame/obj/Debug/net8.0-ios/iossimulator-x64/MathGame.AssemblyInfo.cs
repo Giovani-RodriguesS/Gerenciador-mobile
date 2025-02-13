7761fa098d7fa6a3009a5bad91a687ed33e0699d
Gabriel Brizola <gabrielbrizola13@gmail.com>
2025-02-13 11:38:46 -0300

    feat: remover imagens obsoletas e atualizar referências de ícones na aplicação

--- MathGame/obj/Debug/net8.0-ios/iossimulator-x64/MathGame.AssemblyInfo.cs
+++ MathGame/obj/Debug/net8.0-ios/iossimulator-x64/MathGame.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("MathGame")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0+96b78744e4ac7b67b060d5400d7be4781f08f005")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0+56f31215152117e6caeb2a1eabe5fdfeb855f416")]
 [assembly: System.Reflection.AssemblyProductAttribute("MathGame")]
 [assembly: System.Reflection.AssemblyTitleAttribute("MathGame")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
